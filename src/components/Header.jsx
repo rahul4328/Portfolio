49780ee2515a9918a2253f038718049bff7eb6d2
Rahul Ranjan <rahulranjan4328@gmail.com>
2023-09-12 12:02:58 +0530

    in a tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,23 +11,19 @@ const Header = () => {
           <a href="#home" rel="noreferrer">Home</a>
           <a href="#skills" rel="noreferrer">Skills</a>
           <a href="https://docs.google.com/document/d/1xoP58kh4KgN3QjUD51FBTp9E6JacIxBv4xNauWckqGc/edit?usp=sharing" target='_blank' rel="noreferrer">Resume</a>
-          <a href="#contact" rel="noreferrer>Contact</a>
+          <a href="#contact" rel="noreferrer">Contact</a>
         </div>
       </nav>
       <section className='middle' id='home'>
         <div className='left-content'>
-              <h2>Hello, I'm Rahul Ranjan</h2>
-              <h1>Frontend developer</h1>
-              <p>As a front-end developer proficient in HTML, CSS, JavaScript and React JS. I am eager to learn new technologies, collaborate
-with a team, and contribute to delivering high-quality products that meet end-user needs. With a strong attention to detail
-and a focus on delivering engaging user experiences, I am committed to excellence in front-end development.</p>
-              
-              <a href="#project" rel="noreferrer">Projects</a>
-              
+          <h2>Hello, I'm Rahul Ranjan</h2>
+          <h1>Frontend developer</h1>
+          <p>As a front-end developer proficient in HTML, CSS, JavaScript and React JS. I am eager to learn new technologies, collaborate with a team, and contribute to delivering high-quality products that meet end-user needs. With a strong attention to detail and a focus on delivering engaging user experiences, I am committed to excellence in front-end development.</p>
+          <a href="#project" rel="noreferrer">Projects</a>
         </div>
         <div>
-         <img src="./Image/photo.jpg" alt="/" className='img1'/> 
-         </div>
+          <img src="./Image/photo.jpg" alt="/" className='img1'/> 
+        </div>
       </section>
     </>
   )
